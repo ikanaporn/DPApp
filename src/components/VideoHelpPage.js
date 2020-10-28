3c541982da30f782f9b4166f349d6ff819a3c240
nittayapa <56644493+nittayapa@users.noreply.github.com>
2020-10-28 18:01:20 +0700

    Add files via upload
    flow

--- src/components/VideoHelpPage.js
+++ src/components/VideoHelpPage.js
@@ -53,21 +53,11 @@ class VideoHelpPage extends React.Component {
                                     width: 70,
                                     height: 50,
                                 }}
-                                source={require("../../assets/img/back.png")}
-                            />
-                            {" "}เพื่อย้อนกลับ{"\n"}
-                            - กด{" "}
-                            <Image
-                                style={{
-                                    width: 70,
-                                    height: 50,
-                                }}
                                 source={require("../../assets/img/next.png")}
                             />
-                            {" "}เพื่อไปยังข้อถัดไป{"\n"}{"\n"}
-                            - สัญญาณเสียงเริ่ม คือเสียง{"\n"}
+                            {" "}เพื่อไปยังข้อถัดไป{"\n"}
+                            - สัญญาณเสียงเริ่ม คือเสียง
                         </Text>
-                        
                         <TouchableOpacity
                             style={{
                                 width: 50,
@@ -82,12 +72,9 @@ class VideoHelpPage extends React.Component {
                                 }}
                                 source={require("../../assets/img/volume.png")}
                             />
-                       
-                        </TouchableOpacity> 
-                        
-                        <Text style={{ fontSize: 16, justifyContent: 'center' }}>
+                        </TouchableOpacity>
+                        <Text style={{ fontSize: 16, }}>
                             {" "}(กดรูปลำโพงเพื่อลองฟังเสียง){"\n"}
-                        
                             - ต่อไปนี้จะเป็นแบบทดสอบที่ท่านจะถูกบันทึกภาพและเสียง เมื่อใบหน้าของท่านไม่อยู่ในกรอบจะมีสัญญาณเตือน ขอให้ท่านขยับใบหน้าให้อยู่ในบริเวณเส้นประที่กำหนด{"\n"}
                             - ใบหน้าของท่านจะถูกปิดด้วยแถบสีเทา
                         </Text>
